50fcc85df2ce63fc83ce70f66d89e8a2bf7a9917
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2021-11-21 15:30:28 +0100

    initial commit

---
+++ src/Clock.elm
@@ -0,0 +1,140 @@
+module Clock exposing (Model, Msg(..), init, main, subscriptions, update, view, viewHand)
+
+-- Show an analog clock for your time zone.
+--
+-- Dependencies:
+--   elm install elm/svg
+--   elm install elm/time
+--
+-- For a simpler version, check out:
+--   https://elm-lang.org/examples/time
+--
+
+import Browser
+import Html exposing (Html)
+import Process
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Task
+import Time
+import List exposing (..)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { zone : Time.Zone
+    , time : Time.Posix
+    }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc ( Time.millisToPosix 0 )
+  , Cmd.batch
+            [ Task.perform UpdateTimeZone Time.here
+            , Task.perform UpdateTime Time.now
+            ]
+  )
+
+
+-- UPDATE
+
+type Msg
+  = UpdateTimeZone Time.Zone
+  | UpdateTime Time.Posix
+  | Delay
+  | GetTime
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UpdateTimeZone newZone ->
+      update GetTime( { model | zone = newZone } )
+
+    GetTime ->
+      ( model, Task.perform UpdateTime Time.now )
+
+    UpdateTime newTime ->
+      update Delay ( { model | time = newTime } )
+
+    Delay ->
+      ( model, delay (timeFraction model) )
+
+delay : Float -> Cmd Msg
+delay delayPeriod
+  = Process.sleep delayPeriod
+  |> Task.perform (\_ -> GetTime)
+
+timeFraction : Model -> Float
+timeFraction model =
+    1000 - toFloat (Time.toMillis model.zone model.time)
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+ Sub.none
+
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        hour = toFloat (Time.toHour model.zone model.time)
+        minute = toFloat (Time.toMinute model.zone model.time)
+        second = toFloat (Time.toSecond model.zone model.time)
+    in
+    svg
+        [ viewBox "0 0 400 400"
+        , width "400"
+        , height "400"
+        ]
+        ( circle [ cx "200", cy "200", r "120", fill "#1293D8" ] []
+        :: strokes ++
+        [ circle [ cx "200", cy "200", r "100", fill "#1293D8" ] []
+        , viewHand 8 60 ((hour / 12)  + (minute / 720)) "white"
+        , viewHand 6 90 ((minute / 60) + (second / 3600)) "darkgrey"
+        , viewHand 1 98 (second / 60) "black"
+        ])
+
+strokes : List (Svg msg)
+strokes = List.map ( \num -> viewHand 1 110 ( num/12 ) "white" ) [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+
+viewHand : Int -> Float -> Float -> String -> Svg msg
+viewHand width length turns color =
+    let
+        t = 2 * pi * (turns - 0.25)
+        x = 200 + length * cos t
+        y = 200 + length * sin t
+    in
+    line
+        [ x1 "200"
+        , y1 "200"
+        , x2 (String.fromFloat x)
+        , y2 (String.fromFloat y)
+        , stroke color
+        , strokeWidth (String.fromInt width)
+        , strokeLinecap "round"
+        ]
+        []
---
+++ src/DigitalClock.elm
@@ -0,0 +1,134 @@
+module DigitalClock exposing (..)
+
+import Browser
+import Html exposing (..)
+import Process
+import Task
+import Time exposing (..)
+
+
+
+-- MAIN
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc ( Time.millisToPosix 0 )
+  , Cmd.batch
+            [ Task.perform UpdateTimeZone Time.here
+            , Task.perform UpdateTime Time.now
+            ]
+  )
+
+
+-- UPDATE
+
+type Msg
+  = UpdateTimeZone Time.Zone
+  | UpdateTime Time.Posix
+  | Delay
+  | GetTime
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UpdateTimeZone newZone ->
+      update GetTime( { model | zone = newZone } )
+
+    GetTime ->
+      ( model, Task.perform UpdateTime Time.now )
+
+    UpdateTime newTime ->
+      update Delay ( { model | time = newTime } )
+
+    Delay ->
+      ( model, delay (timeFraction model) )
+
+delay : Float -> Cmd Msg
+delay delayPeriod
+  = Process.sleep delayPeriod
+  |> Task.perform (\_ -> GetTime)
+
+timeFraction : Model -> Float
+timeFraction model =
+    1000 - toFloat (Time.toMillis model.zone model.time)
+
+
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+ Sub.none
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    hour   = String.fromInt (Time.toHour   model.zone model.time)
+    minute = String.fromInt (Time.toMinute model.zone model.time) |> String.padLeft 2 '0'
+    second = String.fromInt (Time.toSecond model.zone model.time) |> String.padLeft 2 '0'
+    year = String.fromInt (Time.toYear model.zone model.time) |> String.padLeft 2 '0'
+    monthName = toDutchMonth (Time.toMonth model.zone model.time) |> String.padLeft 2 '0'
+    monthNumber = String.fromInt (toMonthNumber (Time.toMonth model.zone model.time)) |> String.padLeft 2 '0'
+    day = String.fromInt (Time.toDay model.zone model.time) |> String.padLeft 2 '0'
+  in
+  div []
+     [ h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+     , h1 [] [ text (day ++ " " ++ monthName ++ " " ++ year) ]
+     , h1 [] [ text (year ++ "-" ++ monthNumber ++ "-" ++ day) ]
+     , p  [] [ text ("Lengt of the delayPeriod: " ++ (String.fromFloat (timeFraction model)))]
+    ]
+
+toDutchMonth : Month -> String
+toDutchMonth month =
+  case month of
+    Jan -> "januari"
+    Feb -> "februari"
+    Mar -> "maart"
+    Apr -> "april"
+    May -> "mei"
+    Jun -> "juni"
+    Jul -> "juli"
+    Aug -> "augustus"
+    Sep -> "september"
+    Oct -> "oktober"
+    Nov -> "november"
+    Dec -> "december"
+
+toMonthNumber : Month -> Int
+toMonthNumber month =
+  case month of
+    Jan -> 1
+    Feb -> 2
+    Mar -> 3
+    Apr -> 4
+    May -> 5
+    Jun -> 6
+    Jul -> 7
+    Aug -> 8
+    Sep -> 9
+    Oct -> 10
+    Nov -> 11
+    Dec -> 12
