7a6b56ba2759e15be7d3a0c485922d924867e8c6
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2021-11-27 10:51:30 +0100

    added TimeText, translated into English

--- README.md
+++ README.md
@@ -16,14 +16,16 @@ of the second to wake up the at exactly (haha) the start of the second. It requi
 
 In the `DigitalClock` I publish the delay. It is mostly just under 1000 milli seconds.
 
-To compile the Binary Mondriaan clock (`Main.elm`), run `elm-app build` followed by `serve build` (where `serve` can be replaced by any webserver.
-
 The Binary Mondriaan  clock is divided into an imaginary grid of 8 x 8, so with an area of 64. In that grid, the clock is divided into 7 sections with areas of 1, 2, 4, 8, 16 or 32. The dark gray box in the middle doesn't count. Note that the areas are powers of 2, hence "binary" in the name. Any number between 0 and 63 can be written in 1 unique way as the sum of 0, 1, 2, 3, 4, 5 or 6 of these numbers. For the clock, the numbers 0 to 23 are used for the hours, and 0 to 59 for the minutes, as follows:
 
 - The hours are determined by the area of the red + blue boxes.
 - The minutes are determined by the area of the yellow + blue boxes.
 - The white squares (and the dark gray box in the middle) are NOT included in the count.
 
-A javascript version of the clock can be found on https://github.com/rimvanvliet/mondriaan-klok and runs on vliet.io. As the color clock is not easy to read, I have put the time in (Dutch) text next to it. The text clock is a 12 hour clock, the color clock is a 24 hour clock.
+To compile the Binary Mondriaan clock (`Main.elm`), run `elm-app build` followed by `serve build` (where `serve` can be replaced by any webserver.
+
+To run the Binary Mondriaan clock, run `elm start`.
+
+A javascript version of the clock can be found on https://github.com/rimvanvliet/mondriaan-klok and runs on http://vliet.io. As the color clock is not easy to read, I have put the time in (Dutch) text next to it. The text clock is a 12 hour clock, the color clock is a 24 hour clock.
 
 
--- src/Main.elm
+++ src/Main.elm
@@ -29,17 +29,13 @@ main =
 type alias Model =
   { zone : Time.Zone
   , time : Time.Posix
-  , fraction : Float
   }
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model Time.utc ( Time.millisToPosix 0 ) 0
-  , Cmd.batch
-            [ Task.perform UpdateTimeZone Time.here
-            , Task.perform UpdateTime Time.now
-            ]
+  ( Model Time.utc ( Time.millisToPosix 0 )
+   , Task.perform UpdateTimeZone Time.here
   )
 
 
@@ -55,16 +51,16 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     UpdateTimeZone newZone ->
-      ( { model | zone = newZone }, Cmd.none )
+      update GetTime { model | zone = newZone }
+
+    GetTime ->
+      ( model, Task.perform UpdateTime Time.now )
 
     UpdateTime newTime ->
       update Delay ( { model | time = newTime } )
 
     Delay ->
-      ( { model | fraction = (timeFraction model ) }, delay (timeFraction model) )
-
-    GetTime ->
-      ( model, Task.perform UpdateTime Time.now )
+      ( model, delay (timeFraction model) )
 
 delay : Float -> Cmd Msg
 delay delayPeriod
@@ -83,38 +79,77 @@ timeFraction model =
 baseColor : Array String
 baseColor = Array.fromList ["white","yellow","red","blue"]
 
+clockNames = Array.fromList ["twelve","one","two","three","four","five","six","seven","eight","nine","ten","eleven",
+              "twelve","thir-\nteen","four-\nteen","a quarter","six-\nteen","seven-\nteen","eight-\nteen","nine-\nteen",
+              "twenty", "twenty-\none", "twenty-\ntwo", "twenty-\nthree", "twenty-\nfoour", "twenty-\nfive",
+              "twenty-\nsix", "twenty-\nseven", "twenty-\neight", "twenty-\nnine", "half"]
+
+getClockName : Int -> String
+getClockName index = withDefault "unknown" (get index clockNames)
+
 toRadixBase2 : Int -> List Int
 toRadixBase2 num = RadixInt.toList (RadixInt.fromInt (Base 2) num )
-
-setBackgroundColor : Int -> Array String -> Attribute msg
-setBackgroundColor index array = style "background-color" ( withDefault "white" ( get index array ) )
 
 view : Model -> Html Msg
 view model =
     let
-       hourRadix2 =  List.append ( toRadixBase2 ( Time.toHour model.zone model.time ) ) ( List.repeat 6 0  )
-       minuteRadix2 =  List.append ( toRadixBase2 ( Time.toMinute model.zone model.time ) ) ( List.repeat 6 0  )
+       hour = Time.toHour model.zone model.time
+       minute = Time.toMinute model.zone model.time
+
+       hourRadix2 =  List.append ( toRadixBase2 hour ) ( List.repeat 5 0  )
+       minuteRadix2 =  List.append ( toRadixBase2 minute ) ( List.repeat 5 0  )
+
        clockColorIndex = List.map2 (\h m -> 2 * h + m ) hourRadix2 minuteRadix2
        clockColor = Array.fromList (List.map (\num -> withDefault "white" ( get num baseColor ) ) clockColorIndex)
+       setBackgroundColor index = style "background-color" ( withDefault "white" ( get index clockColor ) )
+
+       minuteName = if (minute == 0) then ""
+         else if (minute <= 30) then ( getClockName minute ++ "\npast\n" )
+         else ( getClockName (60 - minute) ++ "\nto\n" )
+       hourName = if (minute == 0) then getClockName (modBy 12 hour) ++ "\no'clock"
+         else if (minute <= 30) then getClockName (modBy 12 hour)
+         else getClockName ((modBy 12 hour) + 1)
+       timeText = List.intersperse (br [] [])
+               (List.map text
+                   (String.lines ("it's\n" ++ minuteName ++ hourName ))
+               )
     in
     div [ class "box" ] [
         div [ class "container left-half box brdr" ] [
             div [ class "upper-half box" ] [
                 div [ class "left-half box" ] [
-                    div [ setBackgroundColor 3 clockColor, class "upper-half box brdr" ] [],
+                    div [ setBackgroundColor 3, class "upper-half box brdr" ] [],
                     div [ class "lower-half box" ] [
-                        div [ setBackgroundColor 2 clockColor, class "left-half brdr" ] [],
+                        div [ setBackgroundColor 2, class "left-half brdr" ] [],
                         div [ class "right-half box brdr" ] [
                             div [ class "upper-half box" ] [
                                 div [ class "left-half brdrr" ] [],
-                                div [ setBackgroundColor 0 clockColor, class "right-half" ] []
+                                div [ setBackgroundColor 0, class "right-half" ] []
                             ],
-                            div [ setBackgroundColor 1 clockColor, class "lower-half brdrt" ] []
+                            div [ setBackgroundColor 1, class "lower-half brdrt" ] []
                         ]
                     ]
                 ],
-                div [ setBackgroundColor 4 clockColor, class "right-half brdr" ] []
+                div [ setBackgroundColor 4, class "right-half brdr" ] []
             ],
-            div [ setBackgroundColor 5 clockColor, class "lower-half box brdr" ] []
+            div [ setBackgroundColor 5, class "lower-half box brdr" ] []
+        ],
+        div [ class "right-half" ] [
+            div [ class "info" ] [
+                div [ class "tooltip" ] [
+                    div [ class "tooltiptext" ] [
+                        h2 [] [ text "The binary Mondrian clock" ],
+                        p [] [ text "The clock is divided into an imaginary grid of 8 x 8, so with an area of 64." ],
+                        p [] [ text "In that grid, the clock is divided into 7 squares with areas of 1, 2, 4, 8, 16 or 32." ],
+                        p [] [ text "The dark gray box in the middle does not count." ],
+                        p [] [ text "The hours are determined by the area of the red + blue boxes." ],
+                        p [] [ text "The minutes are determined by the area of the yellow + blue boxes." ],
+                        p [] [ text "Because the color clock is not easy to read, I have put the time in text next to it. The text clock is a 12 hour clock, the color clock is a 24 hour clock." ],
+                        p [] [ text "The clock was made in 2021 by Ruud van Vliet." ]
+                    ],
+                    img [ src "info.png", alt"info" ] []
+                ]
+            ],
+            p [ id "timeText" ] timeText
         ]
     ]
